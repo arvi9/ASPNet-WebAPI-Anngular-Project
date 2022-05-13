5cc4d1f588ec141631c7e5d493153c5bab0ad2d6
Manimaran <Manimaran.0610@gmail.com>
2022-05-13 09:55:06 +0530

    Fixed bug in Base Controller

--- Aurora/Source/Aspireoverflow/Controllers/BaseController.cs
+++ Aurora/Source/Aspireoverflow/Controllers/BaseController.cs
@@ -3,9 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AspireOverflow.Controllers
 {
+   
    public class BaseController: ControllerBase
    {
-      public object Message(string message,object? obj=null){
+
+      
+      protected static object Message(string message,object? obj=null){
            if(Message !=null && obj ==null ) return new {Message=message};
            else if(Message !=null && obj !=null) return new{Message=message,DataPassed =obj};
            else return new {};   
