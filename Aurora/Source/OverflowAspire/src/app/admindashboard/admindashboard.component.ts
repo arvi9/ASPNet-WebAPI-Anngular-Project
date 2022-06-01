dd01b5fc18a1796ac19ac9c5a1a4c9f63f9f2620
Sandy-27 <sandhiya270101@gmail.com>
2022-06-01 17:35:22 +0530

    updated admindashbord component

--- Aurora/Source/OverflowAspire/src/app/admindashboard/admindashboard.component.ts
+++ Aurora/Source/OverflowAspire/src/app/admindashboard/admindashboard.component.ts
@@ -4,42 +4,59 @@ import { Dashboard } from 'Models/Dashboard';
 import { application } from 'Models/Application'
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
-
+import {Chart} from 'chart.js';
 @Component({
   selector: 'app-admindashboard',
   templateUrl: './admindashboard.component.html',
   styleUrls: ['./admindashboard.component.css']
 })
 export class AdmindashboardComponent implements OnInit {
-
-  @Input() Usersrc : string=`${application.URL}/Dashboard/GetAdminDashboard`;
-
-
-
-  constructor(private http: HttpClient,private route:Router){}
-
+  constructor(private http: HttpClient) { }
   ngOnInit(): void {
-
-
-
-    if(!AuthService.IsAdmin()) this.route.navigateByUrl("")  //if u want u can navigate to 404 page
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${AuthService.GetData("token")}`
     })
     console.log(AuthService.GetData("token"))
-
-   this.http
-    .get<any>(this.Usersrc,{headers:headers})
-    .subscribe((data)=>{
-      this.data =data;
-
-      console.log(data);
+    this.http
+      .get<any>(`${application.URL}/Dashboard/GetAdminDashboard`,{headers:headers})
+      .subscribe((data) => {
+        this.piedata = data;
+        console.log(data);
+        var names=['Number of Articles', 'Number of Queries'];
+        var details=[];
+        details.push(data.totalNumberOfArticles);
+        details.push(data.totalNumberofQueries);
+      
+     console.log(data.totalNumberOfArticles)
+      
+  
+      new Chart('piechart',{
+        
+        type:'pie',
+        data:{
+        labels:names,
+        datasets:[{
+          data:details,
+  
+        
+        }]
+      },
+      options: {
+        plugins: {
+          legend: {
+            position: 'top',
+            display:true,
+          },
+        },
+        
+      }
     });
-
-
-    }
-
-  public data: Dashboard=new Dashboard();
-
-}
+  });
+  
+   
+  }
+  public piedata: Dashboard=new Dashboard();
+  
+  }
+  
