818cbe7e8b9ff8f76c92faa74271aeea7250189b
Sandy-27 <sandhiya270101@gmail.com>
2022-06-15 15:36:01 +0530

    updated register

--- Aurora/Source/OverflowAspire/src/app/register/register.component.ts
+++ Aurora/Source/OverflowAspire/src/app/register/register.component.ts
@@ -4,8 +4,9 @@ import { Router } from '@angular/router';
 import { application } from 'Models/Application';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AuthService } from '../auth.service';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
+import { data } from 'jquery';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-register',
@@ -13,81 +14,37 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  IsLoading:boolean=false;
+  IsLoading: boolean = false;
   Registersrc: string = `${application.URL}/User/CreateUser`;
 
   designationDetails: any;
   departmentDetails: any;
   GenderDetails: any;
+
   Designationlist: any[] = []
   Designationlist1: any[] = []
-  user = new FormGroup({
-    fullName: new FormControl('', [Validators.required, Validators.maxLength(20), Validators.pattern("^[A-Za-z ]+$")]),
-    gender: new FormControl('', [Validators.required]),
-    aceNumber: new FormControl('', [Validators.required, Validators.pattern("ACE+[0-9]{4}")]),
-    departmentValidate: new FormControl('', [Validators.required]),
-    emailAddress: new FormControl('', [Validators.required, Validators.pattern("([a-zA-Z0-9-_\.]+)@(aspiresys.com|aspiresystems.biz)")]),
-    DesignationValidate: new FormControl('', [Validators.required]),
-    dateOfBirth: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+  user = this.fb.group({
+    fullName: ['', [Validators.required, Validators.maxLength(20), Validators.pattern("^[A-Za-z ]+$")]],
+    gender: ['', [Validators.required]],
+    aceNumber: ['', [Validators.required, Validators.pattern("ACE+[0-9]{4}")]],
+    departmentValidate: ['', [Validators.required]],
+    emailAddress: ['', [Validators.required, Validators.pattern("([a-zA-Z0-9-_\.]+)@(aspiresys.com)")]],
+    DesignationValidate: ['', [Validators.required]],
+    dateOfBirth: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.maxLength(10)]],
 
   })
+  todayAsString: any;
 
-  get fullName() {
-    return this.user.get('fullName');
-  }
-  get gender() {
-    return this.user.get('gender');
-  }
-  get aceNumber() {
-    return this.user.get('aceNumber')
-  }
-  get departmentValidate() {
-    return this.user.get('departmentValidate')
+  age: Number = 0;
+  year: number = 0;
 
-  }
-  get emailAddress() {
-    return this.user.get('emailAddress')
-  }
-  get DesignationValidate() {
-    return this.user.get('DesignationValidate')
-  }
-  get dateOfBirth() {
-    return this.user.get('dateOfBirth')
-  }
-  get password() {
-    return this.user.get('password')
-  }
-  constructor(private http: HttpClient, private router: Router) { }
-  // user: any = {
-  //   userId: 0,
-  //   fullName: '',
-  //   genderId: 0,
-  //   aceNumber: '',
-  //   emailAddress: '',
-  //   password: '',
-  //   dateOfBirth: new Date(),
-  //   verifyStatusID: 3,
-  //   isReviewer: false,
-  //   userRoleId: 2,
-  //   designationId: 0,
-  //   createdOn: Date.now,
-  //   updatedBy: 0,
-  //   updatedOn: null,
-  //   designation: null,
-  //   gender: null,
-  //   userRole: null,
-  //   verifyStatus: null,
-  //   queries: null,
-  //   queryComments: null,
-  //   articleComments: null,
-  //   articles: null,
-  //   likes: null
-  // }
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) { }
+
 
   dobmessage: string = ''
   message: string = ''
-
+  minage: Number = 18;
   validateGendererror = true;
   validateGender(value: any) {
     if (value === 'default') {
@@ -133,8 +90,9 @@ export class RegisterComponent implements OnInit {
     this.Designationlist = [];
     for (let item of this.designationDetails) {
       console.log(item.departmentId)
-      if (item.departmentId == this.departmentValidate?.value) {
-        console.log(item.departmentId)
+      if (item.departmentId == this.user.value['departmentValidate']) {
+        console.log(this.user.value['departmentValidate'])
+
         this.Designationlist.push(item)
         console.log(item)
       }
@@ -142,35 +100,63 @@ export class RegisterComponent implements OnInit {
   }
 
   validateDateOfBirth() {
-    if (Date.now.toString() > this.dateOfBirth?.value) {
-      this.dobmessage = "Date of birth should be valid";
-      console.log(Date.now)
-      console.log(this.dateOfBirth?.value);
-      console.log(this.dobmessage);
-      // return true;
+    this.todayAsString = formatDate(new Date(), 'yyyy-MM-dd', 'en');
+    if (this.user.value['dateOfBirth'] < this.todayAsString) {
+      this.year = parseInt(this.user.value['dateOfBirth']);
+      if ((new Date().getFullYear() - this.year) <= 18) {
+        return true;
+      }
+      return false;
+
     }
-    //  else{
-    //    return false;
-    //  }
+    else {
+      return true;
+    }
   }
 
 
   userdata() {
-    this.IsLoading=true
+    this.IsLoading = true
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
 
     })
     console.log(AuthService.GetData("token"))
-    console.log(this.user)
-    this.http.post<any>(`${application.URL}/User/CreateUser`, this.user, { headers: headers })
+    const registeruser = {
+      userId: 0,
+      fullName: this.user.value['fullName'],
+      genderId: this.user.value['gender'],
+      aceNumber: this.user.value['aceNumber'],
+      emailAddress: this.user.value['emailAddress'],
+      password: this.user.value['password'],
+      dateOfBirth: this.user.value['dateOfBirth'],
+      verifyStatusID: 3,
+      isReviewer: false,
+      userRoleId: 2,
+      designationId: this.user.value['DesignationValidate'],
+      createdOn: Date.now,
+      updatedBy: 0,
+      updatedOn: null,
+      designation: null,
+      gender: null,
+      userRole: null,
+      verifyStatus: null,
+      queries: null,
+      queryComments: null,
+      articleComments: null,
+      articles: null,
+      likes: null
+    }
+    console.log(registeruser);
+    console.log(application.URL);
+    this.http.post<any>(`${application.URL}/User/CreateUser`, registeruser, { headers: headers })
       .subscribe((data) => {
 
         console.log(data)
-        this.router.navigateByUrl("");
+
 
       });
-
+    this.router.navigateByUrl("");
   }
 
 
