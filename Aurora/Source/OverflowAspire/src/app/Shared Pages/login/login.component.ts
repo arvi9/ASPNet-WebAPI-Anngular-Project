a6885cd85c158dc05fd20171e01761792470c322
Girishkumar ravichandran <51507722+Girish0212@users.noreply.github.com>
2022-07-25 15:34:51 +0530

    Updated login page

--- Aurora/Source/OverflowAspire/src/app/Shared Pages/login/login.component.ts
+++ Aurora/Source/OverflowAspire/src/app/Shared Pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/Services/auth.service';
 import { Toaster } from 'ngx-toast-notifications';
@@ -19,8 +19,9 @@ export class LoginComponent implements OnInit {
     Email: '',
     Password: '',
   }
+  error = ""
 
-  constructor( private connection: ConnectionService, private route: Router, private toaster: Toaster) { }
+  constructor(private connection: ConnectionService, private route: Router, private toaster: Toaster) { }
 
   ngOnInit(): void {
   }
@@ -33,8 +34,8 @@ export class LoginComponent implements OnInit {
       .subscribe({
         next: (data) => {
           this.IsAdmin = data.isAdmin,
-          this.IsReviewer = data.isReviewer,
-          this.IsVerified = data.isVerified
+            this.IsReviewer = data.isReviewer,
+            this.IsVerified = data.isVerified
           AuthService.SetDataWithExpiry("token", data.token, data.expiryInMinutes)
           AuthService.SetDataWithExpiry("Admin", data.isAdmin.toString(), data.expiryInMinutes)
           AuthService.SetDataWithExpiry("Reviewer", data.isReviewer.toString(), data.expiryInMinutes)
@@ -53,20 +54,25 @@ export class LoginComponent implements OnInit {
               this.IsLoading = false;
               this.route.navigateByUrl("");
             }
-            if (this.IsVerified == "Approved"){
+            if (this.IsVerified == "Approved") {
               this.route.navigateByUrl("/Home");
             }
           }
         },
         error: (error) => {
           this.IsLoading = false;
-          this.showErrorMessage = true;
+          if (error.status == 0)
+            this.error = "Oops! Server down please try again later";
+          else if (error.status == 500)
+            this.error = "Sorry some internal error occured please try again later";
+          else
+            this.showErrorMessage = true;
         },
-        complete:()=>{
-          this.user.Email=''
-          this.user.Password=''
+        complete: () => {
+          this.user.Email = ''
+          this.user.Password = ''
         }
       });
-      
+
   }
 }
